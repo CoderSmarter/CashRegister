157b1cc052445eca5681fcffc5c8b06680d13269
chuanhuchuang <1028478817@qq.com>
2016-07-17 10:52:29 +0800

    add some information to ProductInfo class

--- src/main/java/ProductInfo.java
+++ src/main/java/ProductInfo.java
@@ -2,4 +2,21 @@
  * Created by Administrator on 2016/7/17.
  */
 public class ProductInfo {
+    private String barcode;
+    private String name;
+    private double price;
+    private double num;   //不能私有化了
+    private String unit;
+    boolean barcodeDisType;
+    boolean barcodeGiveType;
+    ProductInfo(String barcode, String name, double price, double num,
+                String unit, boolean barcodeDisType, boolean barcodeGiveType){
+        this.barcode = barcode;
+        this.name = name;
+        this.price = price;
+        this.num = num;
+        this.unit = unit;
+        this.barcodeDisType = barcodeDisType;
+        this.barcodeGiveType = barcodeGiveType;
+    }
 }
